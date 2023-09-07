7291fa1242fbc09078e1af5942a1da77c503c060
Lily Yang <lily-yangyi@outlook.com>
2023-09-06 22:08:08 -0400

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://lilyyang1:01387251yang@mernproject1.tq34v5o.mongodb.net/'
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParson.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose.connect(db)
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => console.log(`Server started on port ${port}`))
+  
