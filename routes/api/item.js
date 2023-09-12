3e6aae18284df0257ecea90e8c9df150352567e1
Lily Yang <lily-yangyi@outlook.com>
2023-09-11 22:53:02 -0400

    adding express router

--- routes/api/item.js
+++ routes/api/item.js
@@ -7,6 +7,10 @@ const Item = require('../../models/Item')
 //@route GET api/item
 //@desc GET ALL Items
 //@access PUblic
-router.get('/')
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({ date: -1})
+        .then(item => res.json(items));
+})
 
 module.exports = router;
