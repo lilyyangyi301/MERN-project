ea412166fe964a4a890b91921baaf82864914a16
Lily Yang <lily-yangyi@outlook.com>
2023-09-14 00:26:15 -0400

    fix delete api

--- routes/api/items.js
+++ routes/api/items.js
@@ -29,7 +29,7 @@ router.post('/', (req, res) => {
 // @access Public
 router.delete('/:id', (req, res) => {
     Item.findById(req.params.id)
-      .then(item => item.remove().then(() => res.json({ success: true })))
+      .then(item => item.deleteOne().then(() => res.json({ success: true })))
       .catch(err => res.status(404).json({ success: false }));
 });
 
