40a1ac931601cdb6d6e1d3c06adcb921755c6d5c
Lily Yang <lily-yangyi@outlook.com>
2023-09-21 13:47:15 -0400

    add nav bar to the app

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Contrainer
-}  from 'reactstrap';
+	Navbar,
+	NavItem,
+	NavbarToggler,
+	Collapse,
+	NavLink,
+	Nav,
+	NavbarBrand,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+        
+    toggle = () => {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">ShoppingList</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/lilyyangyi301">Github</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        );
+    }
+}
+
+export default AppNavbar;
